0c82905dab2eb7e52c966cb0914335993fd28d91
Daniel-LA0303 <03daniel030399@gmail.com>
2025-06-27 22:03:51 -0600

    add persistence.xml file and producer entity manager

---
+++ src/main/java/com/la/javaee/jaxrs/blog/config/producers/dbConfig/ProducerEntityManager.java
@@ -0,0 +1,20 @@
+package com.la.javaee.jaxrs.blog.config.producers.dbConfig;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.inject.Produces;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+@RequestScoped
+public class ProducerEntityManager {
+	
+	@PersistenceContext()
+	private EntityManager em;
+
+	@Produces
+	@RequestScoped
+	private EntityManager beanEntityManger() {
+		return em;
+	}
+
+}
